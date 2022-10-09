e87cc364ab8922e3caa23a1a1fdb69b6ba370bc7
33-RiveraMary <maryviance.rivera@gmail.com>
2022-10-10 00:14:28 +0800

    Added timer to CollisionScript
    - If score = no. of coin * 10, go to GameWinScene
    - If timer <= 0, go to GameLoseScene

--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -7,20 +7,47 @@ using UnityEngine.SceneManagement;
 public class CollisionScript : MonoBehaviour
 {
     //score
+    public int goal;
     public int score;
     public Text scoreTxt;
+
+    //timer
+    public bool timeRunning;
+    public float time;
+    public Text timeTxt;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        goal = GameObject.FindGameObjectsWithTag("Coin").Length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //timer
+        if (timeRunning)
+        {
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+                DisplayTime(time);
 
+                if (score == goal * 10)
+                {
+                    SceneManager.LoadScene("GameWinScene");
+                }
+            }
+            else
+            {
+                time = 0;
+                DisplayTime(time);
+                timeRunning = false;
+
+                SceneManager.LoadScene("GameLoseScene");
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -37,5 +64,11 @@ public class CollisionScript : MonoBehaviour
         {
             SceneManager.LoadScene("GameLoseScene");
         }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float secs = Mathf.FloorToInt(timeToDisplay % 60);
+        timeTxt.text = "Time Left: " + secs;
     }
 }
